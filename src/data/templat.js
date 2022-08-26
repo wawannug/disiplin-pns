605922c8b31d6a257b9e0f706e2da83598542d2f
kepatuhaninternal <kep.internal@gmail.com>
2022-08-26 16:31:36 +0700

    memperbaiki templatsesuai perka kbn nomor 6 tahun 2022

--- src/data/templat.js
+++ src/data/templat.js
@@ -1,97 +1,171 @@
 export const templat = [
     {
-        id: "A",
-        judul: "Format Pembentukan Tim Pemeriksa",
-        link: "https://docs.google.com/document/d/1aqFJ8-X2nBKs59tyKi-scP7P8lu8Imt1/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
-    {
-        id: "B",
-        judul: "Format Surat Perintah untuk Melakukan Pemeriksaan Pelanggaran Disiplin",
-        link: "https://docs.google.com/document/d/1mpSjkViOdFjl90rC4CRot6rlt4Z9DQqJ/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
-    {
-        id: "C",
-        judul: "Format Surat Pemanggilan dalam Rangka Permintaan Keterangan",
-        link: "https://docs.google.com/document/d/1tz4N_tBH64dz5YKkjzRP4iwwGU2cmS7R/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
-    {
-        id: "D",
-        judul: "Format Surat Undangan dalam Rangka Permintaan Keterangan",
-        link: "https://docs.google.com/document/d/1tz4N_tBH64dz5YKkjzRP4iwwGU2cmS7R/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
-    {
-        id: "E",
-        judul: "Format Berita Acara Permintaan Keterangan",
-        link: "https://docs.google.com/document/d/1o1P9VB5rFGrv2MwPOqXVnFVlPIjN41JC/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
-    {
-        id: "F",
-        judul: "Surat Pernyataan",
-        link: "https://docs.google.com/document/d/1wVpCWAY4fVmoouek47z9oA6pcpOx_Pyy/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
-    {
-        id: "G",
+        id: 1,
         judul: "Surat Panggilan",
-        link: "https://docs.google.com/document/d/1rulpIgTwHFEFhYQuiTFP3rWj4EOCJD-0/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+        link: "https://docs.google.com/document/d/1aMNpbpY9kPXL9BsU9322fJlK756HEs5Z/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true",
     },
     {
-        id: "H",
-        judul: "Format Berita Acara Pemeriksaan",
-        link: "https://docs.google.com/document/d/115eq-IpnZVmGkiAM88qN6Kt39TeD5WQx/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+        id: 2,
+        judul: "Berita Acara Pemeriksaan",
+        link: "https://docs.google.com/document/d/1Qx9pmqoaSjtWcqmX_QGguoALrZJnC0gb/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true",
     },
     {
-        id: "I",
-        judul: "Format Laporan Hasil Pemeriksaan Pelanggaran Disiplin",
-        link: "https://docs.google.com/document/d/1dpSkCRYUqeyf2fH-Yy4r5Gnun8Xpwg9Y/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+        id: 3,
+        judul: "Laporan Hasil Pemeriksaan Pelanggaran Disiplin",
+        link: "https://docs.google.com/document/d/1j39l_m3hxl4w1uoCecssb5eWsweh7ACl/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true",
     },
     {
-        id: "J",
-        judul: "Format Laporan Kewenangan Penjatuhan Hukuman Disiplin",
-        link: "https://docs.google.com/document/d/1AFrBFx7yWzddB962Cy9DFahxN1zPGdMI/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+        id: 4,
+        judul: "Pembentukan Tim Pemeriksa",
+        link: "https://docs.google.com/document/d/1614m33E5b6eA6Q-wAbHji8DCx-X9IlMY/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true",
     },
     {
-        id: "K1",
-        judul: "Format Keputusan Penjatuhan Hukuman Disiplin Teguran Lisan",
-        link: "https://docs.google.com/document/d/1rpEDycAmHXbNg58QU7GH1IbEJ2kKn7zf/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+        id: 5,
+        judul: "Keputusan Pembebasan Sementara Dari Tugas Jabatannya",
+        link: "https://docs.google.com/document/d/1EeoV6vBzumR5svpW0PrYvg6FuPGabyWL/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true",
     },
     {
-        id: "K2",
-        judul: "Format Keputusan Penjatuhan Hukuman Disiplin Teguran Tertulis",
-        link: "https://docs.google.com/document/d/1YuRB4kmKHDulMoQPlIMJpuINlPzF-gFV/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+        id: 6,
+        judul: "Keputusan Hukuman Disiplin Teguran Lisan",
+        link: "https://docs.google.com/document/d/1TwMYoXP0FwwIqu8bhzG5NehnnlSscK4z/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true",
     },
     {
-        id: "K3",
-        judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pernyataan Tidak Puas Secara Tertulis",
-        link: "https://docs.google.com/document/d/1o8ctDMs6G3VI3rC57AnzRuipl_8NB3mt/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+        id: 7,
+        judul: "Keputusan Hukuman Disiplin Teguran Tertulis",
+        link: "https://docs.google.com/document/d/1yQS86ch3ViW86O698HKasYn20-_GQhmB/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
     },
     {
-        id: "K4",
-        judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pemotongan Tunjangan Kinerja sebesar 25% selama 6 bulan",
-        link: "https://docs.google.com/document/d/1AEN_ncovBqOUZ3Xl_biHtlk8_RFvTYRz/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+        id: 8,
+        judul: "Keputusan Hukuman Disiplin Pernyataan Tidak Puas Secara Tertulis",
+        link: "https://docs.google.com/document/d/1hkxGeAmP6HgzG3z2rv9sBzqdJFOHo6I3/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    }, {
+        id: 9,
+        judul: "Keputusan Hukuman Disiplin Pemotongan Tunjangan Kinerja Sebesar 25% selama 6 (enam) bulan",
+        link: "https://docs.google.com/document/d/1bMEm0g0jHBRy7VfUI4fFk5Gx7TXd_387/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    }, {
+        id: 10,
+        judul: "Keputusan Hukuman Disiplin Pemotongan Tunjangan Kinerja Sebesar 25% selama 9 (sembilan) bulan",
+        link: "https://docs.google.com/document/d/1_OQxJtDWvnFhdcu6Nym_2D6Fpivuaw0n/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    }, {
+        id: 11,
+        judul: "Keputusan Hukuman Disiplin Pemotongan Tunjangan Kinerja Sebesar 25% selama 12 (dua belas) bulan",
+        link: "https://docs.google.com/document/d/12K-3tEtUtrQpsFLS1rgYAmPe5v_1ug9s/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    }, {
+        id: 12,
+        judul: "Keputusan Hukuman Disiplin Penurunan Jabatan Setingkat Lebih Rendah Selama 12 (dua belas) bulan",
+        link: "https://docs.google.com/document/d/1md3eyV3o8AInPyh92ZTbkxYTHgv79w3-/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    }, {
+        id: 13,
+        judul: "Keputusan Hukuman Disiplin Pembebasan dari Jabatan Menjadi Jabatan Pelaksana selama 12 (dua belas) bulan",
+        link: "https://docs.google.com/document/d/1b65Z8pnWHv63L3R713E0s4jzpVV1bmpl/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    }, {
+        id: 14,
+        judul: "Keputusan Hukuman Disiplin Pemberhentian Dengan Hormat Tidak Atas Permintaan Sendiri Sebagai PNS",
+        link: "https://docs.google.com/document/d/1bSmNsJNdQPzElxOXe9i98gz21f05-c-h/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    }, {
+        id: 15,
+        judul: "Keputusan Hukuman Disiplin Penurunan Kelas Jabatan Bagi PNS yang Menduduki Jabatan Pelaksana",
+        link: "https://docs.google.com/document/d/1zr13p1jEPRhmup5Pzo0SJa8riDGsSdD4/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    }, {
+        id: 16,
+        judul: "Surat Panggilan Untuk Menerima Keputusan Hukuman Disiplin",
+        link: "https://docs.google.com/document/d/1zr13p1jEPRhmup5Pzo0SJa8riDGsSdD4/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
     },
-    {
-        id: "K5",
-        judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pemotongan Tunjangan Kinerja sebesar 25% selama 9 bulan",
-        link: "https://docs.google.com/document/d/1-GQYNY0BvmwR6rMwFv4pioMiHZDF1ISf/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
-    {
-        id: "K6",
-        judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pemotongan Tunjangan Kinerja sebesar 25% selama 12 bulan",
-        link: "https://docs.google.com/document/d/1CHJ14A8tZKCRvlnSGRVhNFR3yELT1Ulw/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
-    {
-        id: "K7",
-        judul: "Format Keputusan Penjatuhan Hukuman Disiplin Penurunan Jabatan Setingkat Lebih Rendah Selama 12 (dua belas) Bulan",
-        link: "https://docs.google.com/document/d/1CVJnRL3E-kWAb9GIeXFwRsI4KtTPDq1c/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
-    {
-        id: "K8",
-        judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pembebasan dari Jabatannya Menjadi Jabatan Pelaksana Selama 12 (dua belas) Bulan",
-        link: "https://docs.google.com/document/d/1o1ITd7F8lQ3oINbqoXww9iF3SVM6k5gS/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
-    {
-        id: "9",
-        judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pemberhentian Dengan Hormat Tidak Atas Permintaan Sendiri Sebagai PNS",
-        link: "https://docs.google.com/document/d/1TwStYFvAuEDHOVytZTgUBT5g4rgnbbVz/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
-    },
+
+
+    // {
+    //     id: "A",
+    //     judul: "Format Pembentukan Tim Pemeriksa",
+    //     link: "https://docs.google.com/document/d/1aqFJ8-X2nBKs59tyKi-scP7P8lu8Imt1/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "B",
+    //     judul: "Format Surat Perintah untuk Melakukan Pemeriksaan Pelanggaran Disiplin",
+    //     link: "https://docs.google.com/document/d/1mpSjkViOdFjl90rC4CRot6rlt4Z9DQqJ/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "C",
+    //     judul: "Format Surat Pemanggilan dalam Rangka Permintaan Keterangan",
+    //     link: "https://docs.google.com/document/d/1tz4N_tBH64dz5YKkjzRP4iwwGU2cmS7R/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "D",
+    //     judul: "Format Surat Undangan dalam Rangka Permintaan Keterangan",
+    //     link: "https://docs.google.com/document/d/1tz4N_tBH64dz5YKkjzRP4iwwGU2cmS7R/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "E",
+    //     judul: "Format Berita Acara Permintaan Keterangan",
+    //     link: "https://docs.google.com/document/d/1o1P9VB5rFGrv2MwPOqXVnFVlPIjN41JC/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "F",
+    //     judul: "Surat Pernyataan",
+    //     link: "https://docs.google.com/document/d/1wVpCWAY4fVmoouek47z9oA6pcpOx_Pyy/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "G",
+    //     judul: "Surat Panggilan",
+    //     link: "https://docs.google.com/document/d/1rulpIgTwHFEFhYQuiTFP3rWj4EOCJD-0/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "H",
+    //     judul: "Format Berita Acara Pemeriksaan",
+    //     link: "https://docs.google.com/document/d/115eq-IpnZVmGkiAM88qN6Kt39TeD5WQx/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "I",
+    //     judul: "Format Laporan Hasil Pemeriksaan Pelanggaran Disiplin",
+    //     link: "https://docs.google.com/document/d/1dpSkCRYUqeyf2fH-Yy4r5Gnun8Xpwg9Y/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "J",
+    //     judul: "Format Laporan Kewenangan Penjatuhan Hukuman Disiplin",
+    //     link: "https://docs.google.com/document/d/1AFrBFx7yWzddB962Cy9DFahxN1zPGdMI/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "K1",
+    //     judul: "Format Keputusan Penjatuhan Hukuman Disiplin Teguran Lisan",
+    //     link: "https://docs.google.com/document/d/1rpEDycAmHXbNg58QU7GH1IbEJ2kKn7zf/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "K2",
+    //     judul: "Format Keputusan Penjatuhan Hukuman Disiplin Teguran Tertulis",
+    //     link: "https://docs.google.com/document/d/1YuRB4kmKHDulMoQPlIMJpuINlPzF-gFV/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "K3",
+    //     judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pernyataan Tidak Puas Secara Tertulis",
+    //     link: "https://docs.google.com/document/d/1o8ctDMs6G3VI3rC57AnzRuipl_8NB3mt/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "K4",
+    //     judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pemotongan Tunjangan Kinerja sebesar 25% selama 6 bulan",
+    //     link: "https://docs.google.com/document/d/1AEN_ncovBqOUZ3Xl_biHtlk8_RFvTYRz/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "K5",
+    //     judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pemotongan Tunjangan Kinerja sebesar 25% selama 9 bulan",
+    //     link: "https://docs.google.com/document/d/1-GQYNY0BvmwR6rMwFv4pioMiHZDF1ISf/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "K6",
+    //     judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pemotongan Tunjangan Kinerja sebesar 25% selama 12 bulan",
+    //     link: "https://docs.google.com/document/d/1CHJ14A8tZKCRvlnSGRVhNFR3yELT1Ulw/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "K7",
+    //     judul: "Format Keputusan Penjatuhan Hukuman Disiplin Penurunan Jabatan Setingkat Lebih Rendah Selama 12 (dua belas) Bulan",
+    //     link: "https://docs.google.com/document/d/1CVJnRL3E-kWAb9GIeXFwRsI4KtTPDq1c/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "K8",
+    //     judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pembebasan dari Jabatannya Menjadi Jabatan Pelaksana Selama 12 (dua belas) Bulan",
+    //     link: "https://docs.google.com/document/d/1o1ITd7F8lQ3oINbqoXww9iF3SVM6k5gS/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
+    // {
+    //     id: "9",
+    //     judul: "Format Keputusan Penjatuhan Hukuman Disiplin Pemberhentian Dengan Hormat Tidak Atas Permintaan Sendiri Sebagai PNS",
+    //     link: "https://docs.google.com/document/d/1TwStYFvAuEDHOVytZTgUBT5g4rgnbbVz/edit?usp=sharing&ouid=102291095135085919217&rtpof=true&sd=true"
+    // },
 ]
